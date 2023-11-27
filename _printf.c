6cc30e9be851aa27fc161af487a84b9e0e917d0f
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-11-27 05:42:43 -0800

    rewrite _printf for s

--- _printf.c
+++ _printf.c
@@ -5,7 +5,33 @@
 
 int _printf(const char *format, ...)
 {
-	int *ptr = format;
-	printf("sa\n");
-	return (ptr);
+	int i = 0, len = 0;
+	va_list ptr;
+
+	va_start(ptr, format);
+	
+	while (format[len] != '\0')
+	{
+		if ((format[i] == '%') && (format[i + 1] == 'c'))
+		{
+			_putchar(va_arg(ptr, int));
+			i++;
+			len++;
+		}
+		else if ((format[i] == '%') && (format[i + 1] == '%'))
+		{
+			_putchar('%');
+			i++;
+			len++;
+		}
+		else
+		{
+			_putchar(format[i]);
+			len++;
+		}
+		i++;
+	}
+
+	va_end(ptr);
+	return (len);
 }
